1e56136ca4e21968fb6f5a400577c084a874d091
SJ <59878517+twpnpwa@users.noreply.github.com>
2023-09-20 21:26:13 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+      <h1>우리의 첫 프로젝트입니다! 모두 화이팅해서 꼭 완성 시켜봅시다!!!</h1>
+    </div>
+  );
+}
+
+export default App;
