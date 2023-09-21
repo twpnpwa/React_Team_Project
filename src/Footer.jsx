57bda613fd11940dd3d6f3e53d742195ab436ead
zoo02 <rlaguswls9838@icloud.com>
2023-09-21 23:15:33 +0900

    방명록 엔터키 기능 구현

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -30,40 +30,48 @@ const Footer = () => {
     setBody('')
   }
 
+  // 방문자와 내용을 입력하고 엔터를 눌러 방명록에 방문자명과 내용을 추가하는 함수
+  const keyPressHandler = (event) => {
+    if (event.key === 'Enter') {
+      clickAddButtonHandler();
+      event.target.blur() // 내용 입력 후 포커스가 사라지게 하는 메서드
+    }
+  }
 
 
-return (
-  <div>
-    <div className="add-form">
-      <div className="input-group">
-        <label className="form-label">방문자명: </label>
-        <input className="add-input" value={name} onChange={onNameChangeHandler} />
-        <label className="form-label">내용 :</label>
-        <input className="add-input2" value={body} onChange={onBodyChangeHandler} />
+  return (
+    <div>
+      <div className="add-form">
+        <div className="input-group">
+          <label className="form-label">방문자명: </label>
+          <input className="add-input" value={name} onChange={onNameChangeHandler} />
+          <label className="form-label">내용 :</label>
+          <input className="add-input2" value={body} onChange={onBodyChangeHandler}
+            onKeyDown={keyPressHandler} />
+        </div>
+        <button onClick={clickAddButtonHandler} className="add-button">추가하기</button>
       </div>
-      <button onClick={clickAddButtonHandler} className="add-button">추가하기</button>
-    </div>
 
-    <h2 className="list-title">방명록</h2>
+      <h2 className="list-title">방명록</h2>
 
-    <div className="list-wrapper">
+      <div className="list-wrapper">
 
-      {
-        guestbook.map(function (item) {
-        return (
-          <div key={item.id} className="todo-container">
-            <div>
-              <h2 className="todo-title">방문자: {item.name}</h2>
-              <div>내용:{item.body}</div>
-            </div>
+        {
+          guestbook.map(function (item) {
+            return (
+              <div key={item.id} className="todo-container">
+                <div>
+                  <h2 className="todo-title">방문자: {item.name}</h2>
+                  <div>내용:{item.body}</div>
+                </div>
 
-          </div>
-        )
-      })}
+              </div>
+            )
+          })}
 
 
-    </div>
-  </div>)
+      </div>
+    </div>)
 }
   ;
 
