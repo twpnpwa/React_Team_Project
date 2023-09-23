cebbf0d7e72a58c0668aca650a06d6edc858d6b9
soojin0715 <soojin0715@naver.com>
2023-09-23 16:27:34 +0900

    header 디자인 수정

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -18,9 +18,7 @@ const Content = () => {
       <h2 className="yarnbody">퇴근하고 노을지면 우린 이제 시작이다..</h2>
       <span className="yarnbody">a.k.a 직장인 찐광기</span>
       <div class="absolute-rtan-area">
-        {/* <img src="https://cdn.kinandcarta.com/-/media-assets/images/kincarta/insights/2022/02/react-native/react_hero.png?as=0&iar=0&w=1200&rev=61e1dad3af7e465e9544cf8490237772&hash=0AD31383BCBA1DA1C88546327312BA33" onclick="showModal()" /> */}
         <img src="https://styles.redditmedia.com/t5_2su6s/styles/communityIcon_4g1uo0kd87c61.png" onclick="showModal()" />
-
       </div>
 
     </div>
